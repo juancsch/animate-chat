e8197fb83b6e04e5189ad98899c35186175a9852
Juan Carlos Serrano <juancsch@gmail.com>
2016-08-27 18:47:22 +0200

    Complete funtional test

--- test/InitPagesSpec.js
+++ test/InitPagesSpec.js
@@ -4,24 +4,36 @@
 
 'use strict'
 
-describe('Chimp Jasmine', function() {
+describe('Animate chat expectations', function() {
 
-    describe('Page title', function() {
+    describe('Index page', function() {
 
-        it('should be set by the Meteor method @watch', function() {
+        it('should be set initial @watch', function() {
 
             browser.url('http://localhost:8080')
-            expect(browser.getTitle()).to.equal('Chat')
-        });
+            expect(browser.getTitle()).to.equal('Animate Chat')
+            expect(browser.hasFocus('input[name="message"]')).to.equal(true)
+        })
 
-        /* it('should by the Meteor method @watch', function() {
+        it('should show sending message when submit form @watch', function() {
 
             browser.url('http://localhost:8080')
                 .setValue('input[name="message"]', 'my message')
-                .getValue('input[name="message"]')
-                    //.then(function(value) {
-                    //    console.log('***', value)
-                    //})
-        }); */
-    });
-});
+                .submitForm('form[name="sender_msg"]')
+
+            expect(browser.getCssProperty('#alert','display').value)
+                .to.equal('block')
+        })
+
+        xit('should ... @watch', function() {
+
+            const msg = 'a message'
+
+            browser.url('http://localhost:8080')
+                .setValue('input[name="message"]', msg)
+                .submitForm('form[name="sender_msg"]')
+
+            // TODO verify last li has msg
+        })
+    })
+})
