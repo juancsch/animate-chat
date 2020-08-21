a697309d00024bb53cbd0a79531bbd1c887628d0
Juan Carlos Serrano <juancarlos.serrano.chicharro@bbvaitspain.com>
2020-08-21 11:03:01 +0200

    Improved level db database with promise

--- server/level-db/index.js
+++ server/level-db/index.js
@@ -3,41 +3,40 @@ const ttl = require('level-ttl')
 const uuid = require('uuid')
 const concat = require('concat-stream')
 
-const DEFAULT_DURATION = 10 * 60 * 1000
+exports.MessagesLevelDBRepositoryFactory = function ({ databaseName = 'messages.db', duration = 10 * 60 * 1000, limit = 10 } = {}) {
 
-module.exports = function build (options = {limit: 10}) {
-
-	const db = ttl(level('messages.db'), {
+	const db = ttl(level(databaseName), {
 		checkFrequency: 10000
 	})
 
-	const duration = options.duration || DEFAULT_DURATION
-
 	return {
 		save: save(db, duration),
-		list: list(db, options)
+		list: list(db, limit),
+		clear: clear(db)
 	}
 }
 
-const save = (db, duration) => (message = '', callback = () => {}) => {
-
+const save = (db, duration) => message => {
 	const key = `message-${Date.now()}-${uuid.v4()}`
 	const opt = {
 		valueEncoding: 'json',
 		ttl: duration
 	}
 
-	db.put(key, message, opt, callback)
+	return db.put(key, message, opt)
 }
 
-const list = (db, options) => (callback = () => {}) => {
-
-	db.createValueStream({
-		limit: options.limit,
-		valueEncoding: 'json',
-		reverse: true,
-		gt: 'message'
-	}).pipe(concat(function (messages) {
-		callback(null, messages.reverse())
-	})).on('error', callback)
+const list = (db, limit) => () => {
+	return new Promise((resolve, reject) => {
+		db.createValueStream({
+			limit,
+			valueEncoding: 'json',
+			reverse: true,
+			gt: 'message'
+		})
+			.pipe(concat(resolve))
+			.on('error', reject)
+	})
 }
+
+const clear = db => () => db.clear({ gt: 'message' })
